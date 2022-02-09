2e5c2f85085c2c719adc759d1e29babaf72c99ba
nGyu <vpdls1511@gmail.com>
2022-02-09 23:46:52 +0900

    로그인 여부에 따라 다른 정보를 보여주는 버튼 생성

--- src/components/layout/LoginUserInfo.js
+++ src/components/layout/LoginUserInfo.js
@@ -1,6 +1,39 @@
-const LoginUserInfo = () =>{
-  return<div className={"loginUser"}>
+import {useState} from "react";
+import {Link} from "react-router-dom";
 
+const LoginUserInfo = () => {
+
+  const [loginState, setLoginState] = useState(true)
+  const authData = {
+    uid: 1,
+    stuNumber: "202062010",
+    stuName: "김남규"
+  }
+
+  return <div className={"loginUser"}>
+    {
+      loginState ? <LoginTrue data={authData}/> : <LoginFalse/>
+    }
+  </div>
+}
+
+const LoginFalse = () => {
+  return <div className={"loginFalse"}>
+    <Link to={"auth/login"}>로그인</Link>
+  </div>
+}
+
+const LoginTrue = ({data}) => {
+  return <div className={"loginTrue"}>
+    <Link to={"profile/"}>
+      <img
+        src={"https://media.istockphoto.com/vectors/user-member-vector-icon-for-ui-user-interface-or-profile-face-avatar-vector-id1130884625?k=20&m=1130884625&s=612x612&w=0&h=OITK5Otm_lRj7Cx8mBhm7NtLTEHvp6v3XnZFLZmuB9o="}
+        alt={"기본프로필"}/>
+      <div>
+        <p>202062010</p>
+        <p>{data.stuName}</p>
+      </div>
+    </Link>
   </div>
 }
 
